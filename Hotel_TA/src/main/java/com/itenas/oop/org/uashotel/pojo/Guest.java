9db3dd68910abbc472c6c0445a2ffb3b742b4a2c
AyZaku <nicholas.g.i@mhs.itenas.ac.id>
2023-12-25 21:50:27 +0700

    membuat pojo  guest

---
+++ Hotel_TA/src/main/java/com/itenas/oop/org/uashotel/pojo/Guest.java
@@ -0,0 +1,69 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.itenas.oop.org.uashotel.pojo;
+
+/**
+ *
+ * @author Nicholas
+ */
+public class Guest {
+    private String id;
+    private String nama;
+    private String gender;
+    private int pNumber;
+    private int umur;
+    private Account akun;
+
+    public Guest() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getpNumber() {
+        return pNumber;
+    }
+
+    public void setpNumber(int pNumber) {
+        this.pNumber = pNumber;
+    }
+
+    public int getUmur() {
+        return umur;
+    }
+
+    public void setUmur(int umur) {
+        this.umur = umur;
+    }
+
+    public Account getAkun() {
+        return akun;
+    }
+
+    public void setAkun(Account akun) {
+        this.akun = akun;
+    }
+}
