a543f57320a7a0b658db59da1f12a042dd907ebf
Zest <alif.billah122014@gmail.com>
2023-12-25 14:59:45 +0700

    update management class pojo

---
+++ Hotel_TA/src/main/java/com/itenas/oop/org/uashotel/pojo/Management.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.itenas.oop.org.uashotel.pojo;
+
+/**
+ *
+ * @author apple
+ */
+public class Management {
+    private Integer id;
+    private String nama;
+    private boolean loginStatus;
+    private Account akun;
+
+    public Management() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public boolean isLoginStatus() {
+        return loginStatus;
+    }
+
+    public void setLoginStatus(boolean loginStatus) {
+        this.loginStatus = loginStatus;
+    }
+
+    public Account getAkun() {
+        return akun;
+    }
+
+    public void setAkun(Account akun) {
+        this.akun = akun;
+    }
+    
+    
+}
