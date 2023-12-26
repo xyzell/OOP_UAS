b587878e822d9660859e271dbe7adb6430e64655
AyZaku <nicholas.g.i@mhs.itenas.ac.id>
2023-12-26 18:17:52 +0700

    ini mas

--- Hotel_TA/src/main/java/com/itenas/oop/org/uashotel/pojo/Guest.java
+++ Hotel_TA/src/main/java/com/itenas/oop/org/uashotel/pojo/Guest.java
@@ -9,63 +9,70 @@ package com.itenas.oop.org.uashotel.pojo;
  * @author Nicholas
  */
 public class Guest {
-    private String idGuest;
-    private String namaGuest;
-    private String genderGuest;
-    private int pNumberGuest;
-    private int umurGuest;
-    private Account akunGuest;
+    private int ID_Guests;
+    private int ID_Acc; // Foreign Key
+    private String guest_name;
+    private String guest_gender;
+    private String guest_pnumber;
+    private int guest_age;
 
-    public Guest() {
-        
+    // Constructor
+    public Guest(int ID_Guests, int ID_Acc, String guest_name, String guest_gender, String guest_pnumber, int guest_age) {
+        this.ID_Guests = ID_Guests;
+        this.ID_Acc = ID_Acc;
+        this.guest_name = guest_name;
+        this.guest_gender = guest_gender;
+        this.guest_pnumber = guest_pnumber;
+        this.guest_age = guest_age;
     }
 
-    public String getIdGuest() {
-        return idGuest;
+    // Getters and Setters
+    public int getID_Guests() {
+        return ID_Guests;
     }
 
-    public void setIdGuest(String idGuest) {
-        this.idGuest = idGuest;
+    public void setID_Guests(int ID_Guests) {
+        this.ID_Guests = ID_Guests;
     }
 
-    public String getNamaGuest() {
-        return namaGuest;
+    public int getID_Acc() {
+        return ID_Acc;
     }
 
-    public void setNamaGuest(String namaGuest) {
-        this.namaGuest = namaGuest;
+    public void setID_Acc(int ID_Acc) {
+        this.ID_Acc = ID_Acc;
     }
 
-    public String getGenderGuest() {
-        return genderGuest;
+    public String getGuest_name() {
+        return guest_name;
     }
 
-    public void setGenderGuest(String genderGuest) {
-        this.genderGuest = genderGuest;
+    public void setGuest_name(String guest_name) {
+        this.guest_name = guest_name;
     }
 
-    public int getpNumberGuest() {
-        return pNumberGuest;
+    public String getGuest_gender() {
+        return guest_gender;
     }
 
-    public void setpNumberGuest(int pNumberGuest) {
-        this.pNumberGuest = pNumberGuest;
+    public void setGuest_gender(String guest_gender) {
+        this.guest_gender = guest_gender;
     }
 
-    public int getUmurGuest() {
-        return umurGuest;
+    public String getGuest_pnumber() {
+        return guest_pnumber;
     }
 
-    public void setUmurGuest(int umurGuest) {
-        this.umurGuest = umurGuest;
+    public void setGuest_pnumber(String guest_pnumber) {
+        this.guest_pnumber = guest_pnumber;
     }
 
-    public Account getAkunGuest() {
-        return akunGuest;
+    public int getGuest_age() {
+        return guest_age;
     }
 
-    public void setAkunGuest(Account akunGuest) {
-        this.akunGuest = akunGuest;
+    public void setGuest_age(int guest_age) {
+        this.guest_age = guest_age;
     }
 }
 
