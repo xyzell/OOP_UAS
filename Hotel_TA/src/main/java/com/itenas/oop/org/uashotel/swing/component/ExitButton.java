fd9fb8c9e4d8a7b9ebd961de7863f6f93dfb73bf
XZenma <ferorefadhazaidan@gmail.com>
2023-12-30 01:08:51 +0700

    MigLayout kntl

---
+++ Hotel_TA/src/main/java/com/itenas/oop/org/uashotel/swing/component/ExitButton.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.itenas.oop.org.uashotel.swing.component;
+
+import com.itenas.oop.org.uashotel.swing.ExitConfirmation;
+import com.itenas.oop.org.uashotel.swing.loginForm;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Cursor;
+import java.awt.Dialog;
+import java.awt.Frame;
+import java.awt.Image;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import javax.swing.*;
+
+/**
+ * WButton.java
+ * 
+ * An extension of JButton but with custom graphics
+ *
+ */
+
+public class ExitButton extends JButton{
+  ExitConfirmation exit;
+
+  public ExitButton(){
+    setBorder(BorderFactory.createEmptyBorder());
+    setContentAreaFilled(false);
+    setCursor(new Cursor(Cursor.HAND_CURSOR));
+    Check(0);
+    }
+  
+  public void Check (int checker){
+    exit = new ExitConfirmation();
+      addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent me) {
+                if(checker != 1){
+                    
+                exit.setVisible(true);
+                exit.setLocationRelativeTo(null);
+                } else {
+                    exit.toFront();
+                }
+                
+                
+            }
+                
+        });
+  }
+}
+
