98ae624c082c84ebe669e3d7cf0d273ba233ef15
zest <alif.billah122014@Gmail.Com>
2024-01-09 12:59:41 +0700

    update fitur manage guest & room

--- Hotel_TA/src/main/java/com/itenas/oop/org/uashotel/repository/AkunRepository.java
+++
@@ -1,9 +0,0 @@
-
-package com.itenas.oop.org.uashotel.repository;
-
-
-public interface AkunRepository<T, ID> {
-    T login(String username, String password);
-    T register(String username, String email, String password);
-    
-}
